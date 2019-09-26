ddb1b74790498ba7f49af7a8f3253e18e5479f5d
David Andersen <git@davidandersen.us>
2019-09-26 00:33:18 -0700

    more sortino

--- tests/indicator/test_sortino.py
+++ tests/indicator/test_sortino.py
@@ -10,52 +10,74 @@ from mrmkt.common.util import to_date
 
 
 class SortinoIndicator(object):
-    def __init__(self, risk_free_rate: float, minumum_acceptable_return: float):
-        self.minumum_acceptable_return = minumum_acceptable_return
-        self.risk_free_rate = risk_free_rate
+    def __init__(self, dtr: float):
+        """
+        :param dtr: desired target return
+        """
+        self.dtr = dtr
+        # self.risk_free_rate = risk_free_rate
 
     def go(self, series: List[float]):
         slen = len(series)
         # x= -.0582 - .02
         # x=x*x
         # print(x)
+        target_downside_deviation = SortinoIndicator.downside_deviation(series, self.dtr)
+        # monthly_downside_deviation = sqrt(s / (slen - 1))
+        # print(monthly_downside_deviation)
+        # annual_downside_deviation = monthly_downside_deviation * pow(12, .5)
+        # print("annual_downside_deviation=" + str(annual_downside_deviation))
+        average_period_return = sum(series) / len(series)
+
+        print("average_period_return=" + str(average_period_return))
+        print("target return=" + str(self.dtr))
+        print("target_downside_deviation=" + str(target_downside_deviation))
+        # ret = sum(series)
+        # print(ret)
+        numerator = average_period_return - self.dtr
+        print("numerator=" + str(numerator))
+        ratio = (numerator) / target_downside_deviation
+        print("sortino=" + str(ratio))
+        return ratio
+
+    @staticmethod
+    def downside_deviation(returns, target_return):
         negative = []
-        for x in series:
-            delta = x - self.minumum_acceptable_return
+        for data_point in returns:
+            delta = data_point - target_return
+            # negative.append(min(delta, 0))
             if delta < 0:
                 deviationsqrd = delta * delta
                 negative.append(deviationsqrd)
-            # else:
-            #     negative.append(0)
-        s = sum(negative)
-        # print(s)
-        # monthly_downside_deviation = numpy.std(negative)
-        monthly_downside_deviation = sqrt(s / (slen-1))
-        print("monthly_downside_deviation="+str(monthly_downside_deviation))
-        # print(monthly_downside_deviation)
-        annual_downside_deviation = monthly_downside_deviation * pow(12, .5)
-        print("annual_downside_deviation="+str(annual_downside_deviation))
-        annual_return = 1
-        for x in series:
-            annual_return = annual_return * (1 + x)
-        annual_return = annual_return - 1
-        print("annual_return="+str(annual_return))
-        # ret = sum(series)
-        # print(ret)
-        ratio = (annual_return - self.risk_free_rate) / annual_downside_deviation
+                # negative.append(delta)
+            else:
+                negative.append(0)
 
-        return ratio
+        print(negative)
+        s = sqrt(sum(negative) / len(returns))
+        # print(s)
+        # series.add()
+        # downside_deviation = numpy.std(negative)
+        return s
+
+    # def monthly_return_to_annual_return(self, series):
+    #     annual_return = 1
+    #     for x in series:
+    #         annual_return = annual_return * (1 + x)
+    #     # annual_return = annual_return ** (12 / len(series)) - 1
+    #     annual_return = annual_return - 1
+    #     return annual_return
 
 
 class TestSortinoIndicator(unittest.TestCase):
     def test_x(self):
         """
-        https://xplaind.com/262577/sortino-ratio
+        https://www.cmegroup.com/education/files/rr-sortino-a-sharper-ratio.pdf
         """
-        goog = [3.32, .77, 9.21, 6.5, -5.82, 2.4, .95, 2.11, 6, .47, 2.45, 11.81]
-        goog = list(map(lambda x: x / 100, goog))
-        z = SortinoIndicator(.05, .02).go(goog)
-        assert_that(z, equal_to(4.9288829456399945))
+        returns = [17, 15, 23, -5, 12, 9, 13, -4]
+        returns = list(map(lambda x: x / 100, returns))
+        z = SortinoIndicator(0).go(returns)
+        assert_that(z, equal_to(4.417261042993861))
 
     def test_y(self):
         """
@@ -63,8 +85,8 @@ class TestSortinoIndicator(unittest.TestCase):
         """
         apple = [12.89, 4.87, -.01, 6.34, -5.72, 3.27, 10.27, -6.02, 9.68, 1.66, -1.52, 1.79]
         apple = list(map(lambda x: x / 100, apple))
-        z = SortinoIndicator(.05, .02).go(apple)
-        assert_that(z, equal_to(2.985580528647642))
+        z = SortinoIndicator(.05 / 12).go(apple)
+        assert_that(z, equal_to(1.0296649721448592))
 
     # def test_z(self):
     #     """
@@ -83,3 +105,15 @@ class TestSortinoIndicator(unittest.TestCase):
     #     apple = list(map(lambda x: x / 100, apple))
     #     z = SortinoIndicator(.5/100, 0).go(apple)
     #     assert_that(z, equal_to(-1.55))
+
+    def test_z(self):
+        """
+        https://www.ig.com/uk/investments/education/2019/05/01/sortino-ratio-explained
+        """
+        apple = [
+            1.91, 7.91, -1.18, 5.89, -1.66, 7.84, 4.30, 6.12, -2.54, 6.04, 3.55, 5.62,
+            1.15, 4.03,  0.40, 13.09, -2.15, 8.88, 3.34, 1.36, 5.18, 2.24
+        ]
+        apple = list(map(lambda x: x / 100, apple))
+        z = SortinoIndicator(0).go(apple)
+        assert_that(z, equal_to(4.443741058651983))
