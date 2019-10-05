c2260efe80a47927069bdb8ed9541743e4121c6d
David Andersen <git@davidandersen.us>
2019-10-05 00:21:57 -0700

    refactor

--- tiingo2.py
+++ tiingo2.py
@@ -3,6 +3,6 @@ import requests
 headers = {
         'Content-Type': 'application/json'
         }
-requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/goog/prices?startDate=2019-07-12&token=fd6ce0ce784cf699ea6b9b74746b5ea7eddfd2fe",
+requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/goog/prices?startDate=2019-07-12&token=",
                                     headers=headers)
 print(requestResponse.json())
