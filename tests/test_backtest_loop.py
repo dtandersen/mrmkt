c540013d0650e372e7987638ddd3725a772c787e
David Andersen <git@davidandersen.us>
2019-12-15 22:59:16 -0800

    backtest loop

---
+++ tests/test_backtest_loop.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class BacktestLoop:
+    def run(self):
+        pass
+
+
+class TestBuffetModel(TestCase):
+    def setUp(self):
+        pass
+
+    def test_loop(self):
+        loop = BacktestLoop()
+        loop.run()
